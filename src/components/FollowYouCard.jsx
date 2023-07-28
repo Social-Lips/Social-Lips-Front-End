66be8875cf2fd600b308b76f110c7c426fe9afb6
deshan68 <arundeshan>
2023-07-28 15:02:47 +0530

    home page profile card added

---
+++ src/components/FollowYouCard.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const FollowYouCard = () => {
+  return (
+    <div className="max-h-full bg-background_light_blue rounded-lg overflow-y-scroll"></div>
+  );
+};
+
+export default FollowYouCard;
