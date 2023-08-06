3bc8b52ef880e6ca9b84032e61215b85f59011d7
deshan68 <arundeshan>
2023-08-06 21:52:24 +0530

    profile screen category navigation section added

--- src/components/ProfileBody.jsx
+++ src/components/ProfileBody.jsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ProfileBody = () => {
+  const [isActive, setIsActive] = useState("Posts");
   return (
     <main>
       {/* navigation */}
-      <section className="flex justify-center items-center"></section>
+      <section className="flex justify-left items-center gap-x-8 my-5">
+        {["Posts", "Photos", "Videos", "About"].map((item) => (
+          <button
+            className={
+              isActive !== item
+                ? "text-[16px] text-[#9A9A9A] cursor-pointer"
+                : "text-white"
+            }
+            onClick={() => setIsActive(item)}
+          >
+            {item}
+          </button>
+        ))}
+      </section>
+      <div className="w-[260px] h-[1px] bg-input_box_gray" />
     </main>
   );
 };
